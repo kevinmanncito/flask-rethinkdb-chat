b02217b96634472bab6606478623e5b87871a769
Kevin Mann <kevinrmann@gmail.com>
2016-01-08 11:16:27 -0700

    Trying to get the changefeed to work with socket io

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, g, render_template, request, redirect, url_for
+from flask import Flask, g, render_template, make_response, request, redirect, url_for, jsonify
 from flask_socketio import SocketIO, send, emit
 from threading import Thread
 import rethinkdb as r
 from rethinkdb import RqlRuntimeError
+import json
+from datetime import datetime
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -17,11 +19,29 @@ app.config.update(dict(
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+
+def watch_chats():
+    print 'Watching db for new chats!'
+    conn = r.connect(host='db', 
+                     port=28015, 
+                     db='chat')
+    feed = r.table("chats").changes().run(conn)
+    for chat in feed:
+        print 'emitting chat: ', type(chat), chat
+        socketio.emit('new_chat', jsonify(**chat))
+
+    if thread is None:
+        thread = Thread(target=watch_chats)
+        thread.setDaemon(True)
+        thread.start()
+
+
 def init_db():
     conn = r.connect(app.config['DB_HOST'], app.config['DB_PORT'])
     try:
         r.db_create(app.config['DB_NAME']).run(conn)
         r.db(app.config['DB_NAME']).table_create('chats').run(conn)
+        r.db(app.config['DB_NAME']).table('chats').index_create('created').run(conn)
         print 'Database setup completed. Now run the app without --setup.'
     except RqlRuntimeError:
         print 'App database already exists. Run the app without --setup.'
@@ -46,25 +66,18 @@ def teardown_request(exception):
 
 @app.route('/chats/', methods=['POST'])
 def create_chat():
-    
-    new_chat = r.table("chats").insert(request.data).run(g.db_conn)
-    return 'data'
+    data = json.loads(request.data)
+    data['created'] = datetime.now(r.make_timezone('00:00'))
+    if data.get('name') and data.get('message'):
+        new_chat = r.table("chats").insert([ data ]).run(g.db_conn)
+        return make_response('success!', 201)
+    return make_response('invalid chat', 401)
 
 @app.route('/', methods=['GET'])
 def list_shows():
-    chats = list(r.table("chats").order_by(index='id').run(g.db_conn))
+    chats = list(r.table("chats").order_by(index=r.desc('created')).run(g.db_conn))
     return render_template('chats.html', chats=chats)
-
-# def watch_changes():
-#     r.connect( "db", 28015).repl()
-#     feed = r.table("tv_shows").changes().run()
-#     for change in feed:
-#         print 'emitting change: ', change
-#         socketio.emit('change', change)
 
 if __name__ == "__main__":
     # Set up rethinkdb changefeeds before starting server
-    # thread1 = Thread(target=watch_changes)
-    # thread1.setDaemon(True)
-    # thread1.start()
     socketio.run(app, host='0.0.0.0', port=8000)
